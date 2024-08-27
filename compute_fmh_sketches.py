c5233ecd407a7d243f87c042e7a79cec48aa4169
Mahmudur <mahmudhera93@gmail.com>
2024-08-27 13:16:42 -0400

    wait until all children finished

--- compute_fmh_sketches.py
+++ compute_fmh_sketches.py
@@ -1,19 +1,37 @@
 import os
+import subprocess
 
-# list all files with extension .fastq in the current directory
+# List all files with extension .fastq in the current directory
 fastq_files = [f for f in os.listdir('./concated_fastqs') if f.endswith('.fastq')]
 
-# k = 21
-# scaled = 1000
-# seed = 42
-# n = 16 threads per file. all files run in parallel
+# Set parameters
+k = 21
+scaled = 1000
+seed = 42
+n = 16  # threads per file
 
+# Create a list to hold all subprocesses
+processes = []
+
+# Spawn processes
 for fastq_file in fastq_files:
     sketch_file_name = fastq_file.replace('.fastq', '.sketch')
-    cmd = f"fracKmcSketch ./concated_fastqs/{fastq_file} {sketch_file_name} --ksize 21 --scaled 1000 --seed 42 --fq --n 16 &"
-    # execute the command
-    os.system(cmd)
+    cmd = [
+        "fracKmcSketch", 
+        f"./concated_fastqs/{fastq_file}", 
+        sketch_file_name, 
+        "--ksize", str(k), 
+        "--scaled", str(scaled), 
+        "--seed", str(seed), 
+        "--fq", 
+        "--n", str(n)
+    ]
+    # Start the process and add it to the list
+    process = subprocess.Popen(cmd)
+    processes.append(process)
 
-# wait for all the sketches to be computed
-os.system("wait")
-print("All sketches computed!")
+# Wait until all child processes are done
+for process in processes:
+    process.wait()
+
+print("All processes have finished.")
