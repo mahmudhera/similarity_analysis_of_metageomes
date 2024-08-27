bfb273244da64d4c66ed7d09df5728d3a299426e
Mahmudur <mahmudhera93@gmail.com>
2024-08-27 12:35:16 -0400

    changed location of concated file

--- concat_files_in_directories.py
+++ concat_files_in_directories.py
@@ -7,7 +7,7 @@ def concatenate_files_in_directory(path):
         
         # Check if it's a directory and its name starts with "SRS"
         if os.path.isdir(dir_path) and directory.startswith("SRS"):
-            output_file = os.path.join(dir_path, f"{directory}.fastq")
+            output_file = os.path.join(path, f"{directory}.fastq")
             
             # use cat command to concatenate all files in the directory
             os.system(f"cat {dir_path}/*.fastq > {output_file}")
